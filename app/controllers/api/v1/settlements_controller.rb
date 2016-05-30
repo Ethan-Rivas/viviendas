01a5655126bd84bf170803fec81689d05262145e
Ethan-Rivas <ethan-rivas@hotmail.com>
2016-05-30 13:59:33 -0500

    settlement model & controller

---
+++ app/controllers/api/v1/settlements_controller.rb
@@ -0,0 +1,50 @@
+class Api::V1::SettlementsController < ApplicationController
+  before_filter :find_settlement, only: [:show, :update, :destroy]
+
+  def index
+    settlements = Settlement.all
+
+    render json: settlements
+  end
+
+  def show
+    render json: @settlement
+  end
+
+  def create
+    settlement = Settlement.create(settlement_params)
+
+    if settlement.valid?
+      render json: settlement
+    else
+      render json: settlement.errors, status: :unprocessable_entity
+    end
+  end
+
+  def destroy
+    @settlement.destroy
+
+    render nothing: true, status: :ok
+  end
+
+  def update
+    @settlement.update(settlement_params)
+
+    if @settlement.valid?
+      render json: @settlement, status: :ok
+    else
+      render json: { errors: @settlement.errors}, status: :unprocessable_entity
+    end
+  end
+
+  private
+  def settlement_params
+    params.require(:settlement).permit([
+      :uso, :dos, :tres, :contract_id
+    ]) if params[:settlement].present?
+  end
+
+  def find_settlement
+    @settlement = Settlement.find(params[:id])
+  end
+end
