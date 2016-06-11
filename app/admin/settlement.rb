8fc28ddda02b8b9f81eadef0b11e57801469bb65
Ethan-Rivas <ethan-rivas@hotmail.com>
2016-06-10 19:27:58 -0500

    Carga masiva de Asentamientos - Primera Versión

--- app/admin/settlement.rb
+++ app/admin/settlement.rb
@@ -1,5 +1,8 @@
 ActiveAdmin.register Settlement do
-  permit_params :owner_name, :address, :geo_x, :geo_y,
+  permit_params :no, :municipio, :nombre, :apellido_materno, :apellido_paterno,
+                :curp, :telefono, :fecha_nacimiento, :cp, :colonia, :localidad, :calle,
+                :num_casa, :cruzamientos, :dia, :mes, :anio, :sifode, :sexo, :resultado,
+                :marginacion, :procedencia, :procede, :observacion, :owner_name,
                 :check_1, :check_2, :check_3, :check_4,
                 :check_5, :check_6, :check_7, :check_8,
                 :check_9, :check_10, :contract_id
@@ -8,13 +11,27 @@ ActiveAdmin.register Settlement do
 
   index do
     selectable_column
-    id_column
-    column :owner_name
-    column :address
-    column :geo_x
-    column :geo_y
+    column :no
+    column :municipio
+    column :nombre
+    column :apellido_materno
+    column :apellido_paterno
+    column :curp
+    column :telefono
+    column :fecha_nacimiento
+    column :cp
+    column :colonia
+    column :localidad
+    column :calle
+    column :num_casa
+    column :cruzamientos
+    column :dia
+    column :mes
+    column :anio
+    column :sifode
+    column :sexo
+    column :resultado
     column :progress
-    column :contract
     actions
   end
 
---
+++ db/migrate/20160609224449_add_new_fields_to_settlements.rb
@@ -0,0 +1,28 @@
+class AddNewFieldsToSettlements < ActiveRecord::Migration
+  def change
+    add_column :settlements, :no, :integer
+    add_column :settlements, :municipio, :string
+    add_column :settlements, :nombre, :string
+    add_column :settlements, :apellido_paterno, :string
+    add_column :settlements, :apellido_materno, :string
+    add_column :settlements, :curp, :string
+    add_column :settlements, :telefono, :bigint
+    add_column :settlements, :fecha_nacimiento, :date
+    add_column :settlements, :cp, :integer
+    add_column :settlements, :colonia, :string
+    add_column :settlements, :localidad, :string
+    add_column :settlements, :calle, :string
+    add_column :settlements, :num_casa, :integer
+    add_column :settlements, :cruzamientos, :integer
+    add_column :settlements, :dia, :string
+    add_column :settlements, :mes, :string
+    add_column :settlements, :anio, :string
+    add_column :settlements, :sifode, :string
+    add_column :settlements, :sexo, :string
+    add_column :settlements, :resultado, :string
+    add_column :settlements, :marginacion, :string
+    add_column :settlements, :procedencia, :string
+    add_column :settlements, :procede, :string
+    add_column :settlements, :observacion, :string
+  end
+end
