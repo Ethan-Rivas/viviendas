1b39d7a02684ba0c8598393470d487b37e1d736e
Ethan-Rivas <ethan-rivas@hotmail.com>
2016-06-21 18:52:55 -0500

    permit geolocation

--- app/admin/settlement.rb
+++ app/admin/settlement.rb
@@ -2,7 +2,8 @@ ActiveAdmin.register Settlement do
   permit_params :package_id, :no, :municipio, :nombre, :apellido_materno, :apellido_paterno,
                 :curp, :telefono, :fecha_nacimiento, :cp, :colonia, :localidad, :calle,
                 :num_casa, :cruzamientos, :dia, :mes, :anio, :sifode, :sexo, :resultado,
-                :marginacion, :procedencia, :procede, :observacion, :owner_name, progress_check_ids: []
+                :marginacion, :procedencia, :procede, :observacion, :owner_name, progress_check_ids: [],
+                :geo_x, :geo_y
 
   active_admin_importable
 
