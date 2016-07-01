09b65f0643c600850136c937977c21f5814c9988
Ethan-Rivas <ethan-rivas@hotmail.com>
2016-07-01 12:00:18 -0500

    username method for user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,12 +2,15 @@ class User < ActiveRecord::Base
   # Include default devise modules.
   devise :database_authenticatable, :registerable,
           :recoverable, :rememberable, :trackable, :validatable
-  belongs_to :contract
   delegate :settlements, to: :contract, allow_nil: true
 
   include DeviseTokenAuth::Concerns::User
 
   before_validation :generate_password
+
+  def username
+    email.split('@').first.upcase
+  end
 
 private
 
