c197aba738d1834026d6d73f43dd49860433fb69
Erik Escobedo <erik@codigojade.com>
2016-07-07 00:03:54 -0500

    Skip attachment validations for pictures.

--- app/models/picture.rb
+++ app/models/picture.rb
@@ -1,6 +1,7 @@
 class Picture < ActiveRecord::Base
   belongs_to :settlement
-  has_attached_file :image
-  has_attached_file :image, :styles => { :medium => "300x300>", :thumb => "100x100>" }, :default_url => "/images/:style/missing.png"
-  validates_attachment :image, content_type: { content_type: /\Aimage\/.*\Z/ }
+
+  has_attached_file :image, :styles => { :medium => "300x300>", :thumb => "100x100>" }
+  validates_attachment_presence :image
+  do_not_validate_attachment_file_type :image
 end
