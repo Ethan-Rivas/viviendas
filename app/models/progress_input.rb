433ca53d8a07ee2da51f3c54308d4da2234807aa
Ethan-Rivas <ethan-rivas@hotmail.com>
2016-07-11 12:17:36 -0500

    validation with error

--- app/models/progress_input.rb
+++ app/models/progress_input.rb
@@ -1,4 +1,14 @@
 class ProgressInput < ActiveRecord::Base
   belongs_to :settlement
   belongs_to :progress_check
+
+  validate :only_forward
+
+private
+
+    def only_forward
+      if self.progress < self.progress_was
+        errors.add(:progress, 'No puede reducirse el progreso.')
+      end
+    end
 end
